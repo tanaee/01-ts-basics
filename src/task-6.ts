23937c4093f29fb0faf9e317f5b4bd12a4f381f6
tanaee <tanaaeeee@gmail.com>
2025-07-25 23:01:32 +0300

    done

---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const firstNum = getFirstElement<number>([1, 2, 3]);
+const firstStr = getFirstElement<string>(["a", "b", "c"]);
+const firstBool = getFirstElement<boolean>([true, false, true]);
